0ae1b4cec45b77c27050be74067c8bfd0b2ede84
chaitanyasailesh <chaitanyasaileshtondepu@gmail.com>
2019-06-04 13:42:39 -0500

    Add files via upload

---
+++ icp1/arithmetic.py
@@ -0,0 +1,12 @@
+def arithmetic(a,b):
+    return a+b
+print(arithmetic(1,2))
+def arithmetic1(c,d):
+    return c-d
+print(arithmetic1(3,4))
+def arithmetic2(e,f):
+    return e*f
+print(arithmetic2(2,5))
+def arithmetic3(g,h):
+    return g/h
+print (arithmetic3(7,8))
